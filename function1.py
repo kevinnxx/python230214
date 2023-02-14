6a583efce851072e1934f12dd720f4bf39579fb9
kevinnxx <kevinlee0438@gmail.com>
2023-02-14 12:54:15 +0900

    두번째

--- function1.py
+++ function1.py
@@ -5,6 +5,7 @@ def times(a,b):
 
 #함수호출
 print(times(3,4))
+print(times(3,4))
 
 #함수정의
 def swap(x,y):
--- 그냥그저그런파일.py
+++ 그냥그저그런파일.py
@@ -4,6 +4,6 @@
 t = 20* 10
 for i in list(range(0,201,20)):
     while t > 0:
-        t -= 1
         if t == i:
-            print(t)
+            print(i)
+            t -= 1
