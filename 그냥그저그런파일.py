7a716cb42911aeaab44d0f2bb401be476559b697
kevinnxx <kevinlee0438@gmail.com>
2023-02-14 13:37:16 +0900

    세번째

--- 그냥그저그런파일.py
+++ 그냥그저그런파일.py
@@ -1,9 +1,7 @@
 #그냥그저그런파일.py
 
 
-t = 20* 10
-for i in list(range(0,201,20)):
-    while t > 0:
-        if t == i:
-            print(i)
-            t -= 1
+a= {1:1, 2:2, 3:3}
+for first,last in a:
+    print(first, last)
+            
