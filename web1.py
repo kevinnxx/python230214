9797ea71db202dee9d41bcf75200275e0c05111d
kevinnxx <kevinlee0438@gmail.com>
2023-02-16 16:56:05 +0900

    .

--- web1.py
+++ web1.py
@@ -3,5 +3,13 @@ from bs4 import BeautifulSoup
 
 page = open("c:\\work\\test01.html", "rt", encoding="utf-8")
 soup = BeautifulSoup(page, "html.parser")
-print(soup.prettify())
+# print(soup.prettify())
+# print(soup.find_all("p"))
+#print(soup.find_all("a"))
+#print(soup.find_all("p", class_ = "outer-text"))
 
+#태그 내부의 문자열: .text 속성
+for tag in soup.find_all("p"):
+    title = tag.text.strip()
+    title = title.replace("\n", "")
+    print(title)
---
+++ web2.py
@@ -0,0 +1,24 @@
+#web2.py
+#웹서버 요청
+import urllib.request
+from bs4 import BeautifulSoup
+
+data = urllib.request.urlopen(
+    "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
+
+soup = BeautifulSoup(data, "html.parser")
+
+cartoons = soup.find_all("P", class_= "title")
+print("갯수:{0}".format(len(cartoons)))
+title = cartoons[0].find("a")
+link = cartoons[0].find("a")["href"]
+print(title)
+print(link)
+
+for item in cartoons:
+    title = item.find("a".text)
+    print(title.strip())
+
+# <td class="title">
+# 	<a href="/webtoon/detail?" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
+#</td>
